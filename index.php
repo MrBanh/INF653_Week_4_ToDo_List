dad140f415f6dae3c8d0aa352c01bc7788362ff9
MrBanh <tonybanh@live.com>
2021-02-10 20:12:02 -0800

    Initial setup of files for to do list app

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ToDo List</title>
+</head>
+<body>
+    <main>
+        <header>
+            <h1>ToDo List</h1>
+        </header>
+
+        <section aria-label="List of To Do Items">
+            <ul>
+                <!-- php foreach loop to pull all todo items from DB -->
+            </ul>
+        </section>
+
+        <section aria-label="Add To Do Item">
+            <h2>Add Item</h2>
+            <!-- Form -->
+            
+
+        </section>
+    </main>
+</body>
+</html>
